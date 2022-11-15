914c21d2b0ba68e5a7dadae84e7b7b4587b0cd97
VISHAL1855 <85866005+VISHAL1855@users.noreply.github.com>
2022-11-15 21:34:30 +0530

    Add files via upload

---
+++ Bank Notes/Marks.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+class Mark(BaseModel):
+    study_hours=float
+
---
+++ Bank Notes/app.py
@@ -0,0 +1,35 @@
+
+import uvicorn
+from fastapi import FastAPI
+from BankNotes import BankNote
+from Marks import Mark
+import pickle
+import numpy as np
+import pandas as pd
+app=FastAPI()
+pickle_in=open("classifier.pkl",'rb')
+classifier = pickle.load(pickle_in)
+@app.get("/")
+def index():
+    return {'message':'Hello,Students'}
+@app.post('/predict')
+def predict_marks(data:BankNote):
+    data = data.dict()
+    variance=data['variance']
+    skewness=data['skewness']
+    curtosis=data['curtosis']
+    entropy=data['entropy']
+    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
+    if(prediction[0]>0.5):
+        prediction="Fake note"
+    else:
+        prediction="Its a Bank note"
+    return {
+        'prediction': prediction
+    }
+if __name__=='__main__'  :
+    uvicorn.run(app,host='127.0.0.1',port=8000)
+
+
+
+
